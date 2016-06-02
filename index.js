8b26656514b92f0e4c9f4a0be11707f9bc3a12be
Jordan Harband <ljharb@gmail.com>
2016-06-02 02:41:26 -0700

    Remove nonstandard transform from ES2015 preset (#2)
    * Remove nonstandard transform from ES2015 preset
    
    * Remove nonstandard transform from ES2015 preset

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* global module */
 module.exports = {
   plugins: [
-    require('babel-plugin-syntax-trailing-function-commas'),
     require('babel-plugin-transform-es2015-modules-commonjs'),
   ],
 };
